08c5f9b14650073fb4a11fe06aa58beeb6fb0517
Metehan Gezer <irrelevantdev@outlook.com>
2023-03-22 12:12:35 +0300

    added webpack copy plugin to copy static files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+ 
+module.exports = {
+    context: path.join(__dirname, 'your-app'),
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'static' }
+            ]
+        })
+    ]
+};
+
 module.exports = {
     entry: './src/program.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'static' }
+            ]
+        })
+    ],
     watch: true
 };
